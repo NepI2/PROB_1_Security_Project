387ec6e096cb795999c87978a7fb5b1ccbef7235
11303432@student.pxl.be <11303432@student.pxl.be>
2023-03-14 12:18:53 +0100

    added RSA class

---
+++ SecurityProject/RSA/RSA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SecurityProject.RSA
+{
+    public class RSA
+    {
+        private RSACryptoServiceProvider rsa;
+        public RSA() 
+        {
+            rsa = new RSACryptoServiceProvider();
+        }
+        public string GetPublicKey()
+        {
+            return rsa.ToXmlString(false);
+        }
+        public string GetPrivateKey()
+        {
+            return rsa.ToXmlString(true);
+        }
+
+        public byte[] Encrypt(string plainText, string publicKey)
+        {
+            rsa.FromXmlString(publicKey);
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] encryptedBytes = rsa.Encrypt(plainBytes, true);
+            return encryptedBytes;
+        }
+
+        public string Decrypt(byte[] encryptedBytes, string privateKey)
+        {
+            rsa.FromXmlString(privateKey);
+            byte[] decryptedBytes = rsa.Decrypt(encryptedBytes, true);
+            string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
+            return decryptedText;
+        }
+    }
+}
